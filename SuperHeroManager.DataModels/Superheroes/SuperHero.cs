aa998074343fdef87391fc817d9949a8029141dc
szabototo89 <szabototo89@gmail.com>
2016-02-10 22:52:40 +0100

    Initial commit

---
+++ SuperHeroManager.DataModels/Superheroes/SuperHero.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections;
+using SuperHeroManager.DataModels.Common;
+
+namespace SuperHeroManager.DataModels.Superheroes
+{
+    public class SuperHero : IEntity
+    {
+        public Int32 Id { get; set; }
+
+        public String Name { get; set; }
+
+        public IEnumerable<Skill> Skills { get; set; }
+
+
+    }
+}
