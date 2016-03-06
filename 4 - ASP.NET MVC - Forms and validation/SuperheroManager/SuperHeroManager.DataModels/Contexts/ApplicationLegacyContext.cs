bf438d6ab81c6afc77a716e7b0ac58999e34a658
szabototo89 <szabototo89@gmail.com>
2016-03-06 14:42:01 +0100

    Add lecture 04

---
+++ 4 - ASP.NET MVC - Forms and validation/SuperheroManager/SuperHeroManager.DataModels/Contexts/ApplicationLegacyContext.cs
@@ -0,0 +1,35 @@
+﻿using System.Data.Entity;
+using SuperHeroManager.DataModels.Superheroes;
+
+namespace SuperHeroManager.DataModels.Contexts
+{
+  public class ApplicationLegacyContext : ApplicationContextBase
+  {
+    public ApplicationLegacyContext() : base("MyLegacySuperheroDatabase")
+    {
+    }
+
+    protected override void OnModelCreating(DbModelBuilder modelBuilder)
+    {
+      var superheroConfiguration = modelBuilder.Entity<SuperHero>();
+
+      superheroConfiguration.HasMany(hero => hero.Teams)
+                            .WithMany(team => team.SuperHeroes);
+
+      superheroConfiguration.HasMany(hero => hero.Skills);
+      superheroConfiguration.HasKey(hero => hero.Id);
+
+      var skillConfiguration = modelBuilder.Entity<Skill>();
+      skillConfiguration.Property(skill => skill.Name)
+                        .HasColumnName("skill_name")
+                        .HasMaxLength(50);
+
+      skillConfiguration.Property(skill => skill.Description)
+                        .HasColumnName("skill_description")
+                        .HasMaxLength(500);
+
+      skillConfiguration.Property(skill => skill.Value)
+                        .HasColumnName("strength_power");
+    }
+  }
+}
