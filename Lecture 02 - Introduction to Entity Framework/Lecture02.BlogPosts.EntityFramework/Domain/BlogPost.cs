6bc98b62968b8d2304af480b0912970858b8b5ad
szabototo89 <szabototo89@gmail.com>
2017-02-27 22:50:14 +0100

    Add lecture 02 to project

---
+++ Lecture 02 - Introduction to Entity Framework/Lecture02.BlogPosts.EntityFramework/Domain/BlogPost.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Runtime.InteropServices.ComTypes;
+using Microsoft.SqlServer.Server;
+
+namespace Lecture02.BlogPosts.EntityFramework.Domain
+{
+    public class BlogPost: IEntity
+    {
+        public Int32 Id { get; set; }
+
+        public String Title { get; set; }
+
+        public String Content { get; set; }
+
+        public DateTime CreatedDate { get; set; }
+
+        public virtual ICollection<Author> Authors { get; set; }
+
+        public override String ToString() 
+            => $"Id: {Id}, Title: {Title}, Content: {Content}, CreatedDate: {CreatedDate}";
+    }
+}
