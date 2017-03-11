39302b696fc7da782517580ad1451d3ecfb20c84
szabototo89 <szabototo89@gmail.com>
2017-03-11 18:19:15 +0100

    Add lecture 03 skeleton to repository

---
+++ Lecture 03 - Introduction to ASP.NET MVC/Lecture03.BlogPosts.EntityFramework/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Lecture02.BlogPosts.EntityFramework.Contexts;
+using Lecture02.BlogPosts.EntityFramework.Domain;
+
+namespace Lecture02.BlogPosts.EntityFramework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String connectionString = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=G:\Development\Playground\Lecture02.BlogPosts.EntityFramework\Lecture02.BlogPosts.EntityFramework\BlogDatabase.mdf;Integrated Security=True;MultipleActiveResultSets=True";
+            using (var context = new LocalDatabaseContext(connectionString))
+            {
+                AddTestData(context);
+                WriteBlogPosts(context);
+            }
+        }
+
+        private static void AddTestData(LocalDatabaseContext context)
+        {
+            var blogPost = new BlogPost
+            {
+                Title = "Test",
+                Content = "Lorem ipsum",
+                CreatedDate = DateTime.Now,
+            };
+
+            var johnDoeAuthor = new Author
+            {
+                FullName = "John Doe",
+                BlogPosts = new[] { blogPost }
+            };
+
+            context.Authors.Add(johnDoeAuthor);
+            context.BlogPosts.Add(blogPost);
+
+            context.SaveChanges();
+        }
+
+        private static void WriteBlogPosts(LocalDatabaseContext context)
+        {
+            foreach (var blogPost in context.BlogPosts.ToArray())
+            {
+                Console.WriteLine(blogPost);
+                foreach (var author in blogPost.Authors.ToArray())
+                {
+                    Console.WriteLine(author);
+                }
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
